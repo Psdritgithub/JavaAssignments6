0f679f3451b20a8dcd23868b66f76e36203eb017
Psdritgithub <127477213+Psdritgithub@users.noreply.github.com>
2024-10-02 23:35:26 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,44 @@
+
+package Assinment6;
+
+public class Person {
+
+	final String name = "rohit";
+
+	final int age = 23;
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public int getAge() {
+
+		return age;
+
+	}
+
+	@Override
+
+	public String toString() {
+
+		return "Person [name=" + name + ", age=" + age + "]";
+
+	}
+
+	public static void main(String[] args) {
+
+		Person person = new Person();
+
+		System.out.println(person.age);
+
+		System.out.println(person.name);
+
+		System.out.println(person.getName());
+
+		System.out.println(person);
+
+	}
+
+}
